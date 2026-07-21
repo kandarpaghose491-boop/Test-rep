7b07c8f0ec0821815e5af3f48029575754e787ce
Ricash <soumojit.dutta1234@gmail.com>
2026-07-22 00:46:45 +0530

    Update tic_tac_toe.c

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 int board[9];
+int draw_check(int a[]){
+    int count=0;
+    for (int i = 0; i < 9; i++)
+    {
+        if (a[i]==0)
+        {
+            count +=1;            
+        }
+        
+    }
+        if (count<1)
+        {
+            return 1;
+        } 
+        return 0;   
+}
 int win_check(int player){
 if (board[0]==player&& board[3]==player&& board[6]==player){return player;}
 if (board[1]==player&& board[4]==player&& board[7]==player){return player;}
