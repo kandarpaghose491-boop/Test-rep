e6a8d350fdbb4161b8783ad1047bb2094da0db00
Ricash <soumojit.dutta1234@gmail.com>
2026-07-21 20:53:39 +0530

    Update tic_tac_toe.c

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-int board[8]={0};
-int win_check(int player){
 
+int win_check(int a[9], int b){
+
+if (a[0]==b && a[3]==b && a[6]==b){return 1;}
+if (a[1]==b && a[4]==b && a[7]==b){return 1;}
+if (a[2]==b && a[5]==b && a[8]==b){return 1;}
+
+if (a[0]==b && a[1]==b && a[2]==b){return 1;}
+if (a[3]==b && a[4]==b && a[5]==b){return 1;}
+if (a[6]==b && a[7]==b && a[8]==b){return 1;}
+
+if (a[0]==b && a[4]==b && a[8]==b){return 1;}
+if (a[2]==b && a[4]==b && a[6]==b){return 1;}
+return 0;
 }
 int main(){
     printf("Hello World");
