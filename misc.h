2263b0627c5c2fdd44b67b5ee3a480b3161019ac
Magnes <kandarpaghose491@gmail.com>
2026-07-23 18:40:23 +0530

    Update misc.h

--- misc.h
+++ misc.h
@@ -4,3 +4,8 @@ int rand_choice_int(int *arr,int len){
 int element=rand()%len;
 return arr[element];
 }
+void arrycpy(int *dest,int *src,int len){
+for(int i=0;i<len;i++){
+    dest[i]=src[i];
+}
+}
